b820595bbdefc68627e0f27177c2a4f3e82d8108
Luís Cardoso <lfac-pt@users.noreply.github.com>
2024-08-19 16:06:08 +0100

    Fix unit test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Clique ou arraste ficheiros para esta área para começar/i);
   expect(linkElement).toBeInTheDocument();
 });
